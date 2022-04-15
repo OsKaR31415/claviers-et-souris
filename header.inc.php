529d39f9e2fd2a158dfa574624c350980448abcd
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-04-15 15:25:35 +0200

    page to add keyboard in DB

--- header.inc.php
+++ header.inc.php
@@ -4,6 +4,7 @@
     <div class="menu_item"><a href="index.php">Accueil</a></div>
     <div class="menu_item"><a href="claviers.php">Claviers</a></div>
     <div class="menu_item"><a href="souris.php">Souris</a></div>
+    <div class="menu_item"><a href="ajouter.php">Ajouter</a></div>
     <div class="menu_item"><a href="articles.php">Articles</a></div>
 </div>
 
