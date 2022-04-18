b2ea7271c516be481fa84352f5cfcf105c311d25
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-04-18 02:53:48 +0200

    Create articles.php

---
+++ articles.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="contents">
+
+        </div>
+    </div>
+
+</body>
+</html>
+
+
+
