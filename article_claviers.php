255a8b0ce1c42b2153d6667463fbfd1d98fbb525
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-04-21 01:52:31 +0200

    Create article_claviers.php

---
+++ article_claviers.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <div class="container">
+        <div class="contents">
+            <h1>Claviers ergonomiques</h1>
+            Les claviers ergonomiques ont des conceptions très variées, et le nombre des combinaisons est presque infini.
+
+            <h2>Les types de claviers</h2>
+            <h3>Les clavier <em>split</em></h3>
+            Les claviers <em>split</em> sont des claviers dont les touches de la main gauche et de la main droite sont séparées, c'est-à-dire qu'un espace ou un angle est mis entre ces deux parties, afin d'améliorer la position des mains.
+
+            
+        </div>
+    </div>
+</body>
+</html>
