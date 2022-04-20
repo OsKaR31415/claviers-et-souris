9ccec09c104aff2ec241bcbc33ad1fe5545813eb
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-04-21 01:52:44 +0200

    articles list page

--- articles.php
+++ articles.php
@@ -8,6 +8,22 @@
 
     <div class="container">
         <div class="contents">
+            <h1>Articles</h1>
+
+            <div class="article_presentation">
+                <h2><a href="article_claviers.php">Claviers Ergonomiques</a></h2>
+                Présentation des différents types de claviers ergonomiques.
+            </div>
+
+            <div class="article_presentation">
+                <h2><a href="article_souris.php">Souris Ergonimiques</a></h2>
+                Présentation des différents types de souris ergonomiques.
+            </div>
+
+            <div class="article_presentation">
+                <h2><a href="article_bdd.php">Informations sur la base de données</a></h2>
+                Informations diverses sur la base de données du site.
+            </div>
 
         </div>
     </div>
