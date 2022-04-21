cc48053c3f096cd136d6e2b1103491534d263605
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-04-21 20:17:11 +0200

    Create article_souris.php

---
+++ article_souris.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Ergoscar - souris ergonomiques</title>
+</head>
+<body>
+    <?php include('header.inc.php') ?>
+    <div class="container">
+        <div class="contents">
+            <h1>Les souris ergonomiques</h1>
+
+            <br> <br> <br> <br>
+            <h3>Les types de souris</h3>
+
+            <br> <br>
+            <h4>Les souris classiques</h4>
+            Les souris classiques ne sont pas à présenter. On peut leur donner l'avantage d'être rapides à manipuler, et d'être précises.
+            Etant donné que ce type de souris est le plus répandu, nous sommes presques tous plus habitués à ce type de souris.
+
+            <br> <br>
+            <h4>Les Trackpads</h4>
+            Les trackpads sont aussi très répandus sur les ordinateurs portables.
+            Sur un ordinateur portable, leur position les rend assez ergonomiques.
+            Il est cependant à remarquer que les trackpads externes aux ordinateurs portables sont assez rares.
+            Si vous pouvez vous en procurer un, ils sont une très bonne solution ergonomique.
+
+            <br> <br>
+            <h4>Les Trackballs</h4>
+            Les trackballs sont beaucoup moins répandues que les deux autres types de souris.
+            Elles ont plusieurs avantages et désavantages.
+            Le principal avantage des trackballs est que, contrairement aux souris classiques, on peut les positionner à un endroit ergonomique, et l'on à pas à bouger le poignet pour l'utiliser (les mouvements du poignet êtant le plus gros problème ergonomique des souris classiques).
+            Un autre avantage est qu'ils mobilisent des muscles qui ne sont pas déjà mobilisés pas l'utilisation d'un clavier, ce qui permet d'éviter l'utilisation répétée des mêmes muscles : que ce soit une trackball au pouce, qui utilise un doigt beaucoup plus musclé que les autres, ou bien une trackball aux doigts, que l'on peut controller avec n'importe quel(s) doigt(s).
+            Le problème est que les trackballs sont moins précises que des souris, mais cela peut être compensé par une touche qui change la vitesse de la trackball, pour alterer entre un mode rapidité, et un mode précision.
+
+
+            <br> <br> <br> <br>
+            <h3>Conclusion</h3>
+            Les trackpads et trackballs ont des avantages ergonomiques, mais sont aussi moins intuitifs et moins précis. Le choix dépend donc de votre usage.
+            Le plus important dans le choix d'une souris est le fait qu'elle convienne ou non à votre main. Le reste est en fait secondaire.
+
+            <a href="souris.php">Liste des souris</a>
+
+
+        </div>
+    </div>
+</body>
+</html>
