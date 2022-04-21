4f2772e47f8b2e543b862a41eca3259eeb29f659
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-04-21 20:17:08 +0200

    Update article_claviers.php

--- article_claviers.php
+++ article_claviers.php
@@ -2,19 +2,61 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <link rel="stylesheet" href="style.css">
+    <title>Ergoscar - Claviers ergonomiques</title>
 </head>
 <body>
+    <?php include('header.inc.php') ?>
     <div class="container">
         <div class="contents">
             <h1>Claviers ergonomiques</h1>
             Les claviers ergonomiques ont des conceptions très variées, et le nombre des combinaisons est presque infini.
 
-            <h2>Les types de claviers</h2>
-            <h3>Les clavier <em>split</em></h3>
+            <br> <br> <br>
+            <h3>Les types de claviers</h3>
+            <br> <br>
+            <h4>Les clavier <em>split</em></h4>
             Les claviers <em>split</em> sont des claviers dont les touches de la main gauche et de la main droite sont séparées, c'est-à-dire qu'un espace ou un angle est mis entre ces deux parties, afin d'améliorer la position des mains.
+            <br>
+            <a href="claviers.php?prix_ou_nb_touches=prix&prix=60&nb_touches=60&split=on&action=Rechercher">Liste des claviers splits</a>
 
-            
+            <br> <br>
+            <h4>Les claviers en deux parties</h4>
+            Les claviers en deux parties sont des claviers <em>split</em> dont les deux parties sont physiquement séparées, afin que l'on puisse ajuster l'espacement et l'angle entre les deux.
+            <br>
+            <a href="claviers.php?prix_ou_nb_touches=prix&prix=60&nb_touches=60&split=on&action=Rechercher">Liste des claviers split</a>
+
+            <br> <br>
+            <h4>Les claviers mécaniques</h4>
+            Les claviers mécaniques sont des claviers dont les touches sont particuière. Elles ont un toucher plus agréable pour taper. De nombreux types de touches mécaniques existent, avec des touchers très différents, mais un clavier mécanique est toujours plus agréable qu'un clavier classique.
+
+            <br> <br> <br> <br>
+            <h3>Les type d'arrangements des touche (<em>Layouts</em>)</h3>
+            Il existe différents arrangements des touches sur les claviers. L'arrangement le plus classique est appelé <em>staggered</em>
+
+            <br> <br>
+            <h4>Les claviers <em>staggered</em></h4>
+            Les claviers <em>staggered</em> sont les claviers dont les touches sont arrangées comme sur un clavier classique, c'est-à-dire qu'elles sont ordonnées en lignes décalées les unes par rapport aux autres.
+
+            <br> <br>
+            <h4>Les claviers orthogonaux</h4>
+            Les claviers orthogonaux sont les claviers dont les touches sont arrangées comme une grille.
+            <br>
+            <a href="claviers.php?prix_ou_nb_touches=prix&prix=60&nb_touches=60&orthogonal=on&action=Rechercher">Liste des claviers orthogonaux</a>
+
+            <br> <br>
+            <h4>Les claviers <em>columnar</em></h4>
+            Les claviers <em>columnar</em> sont une évolution des clavier orthogonaux. Le positionnement des touches est presque le même, à ceci près que la hauteur de certaines colonnes est décalée, afin de mieux correspondre à la longueur des doigts (les doigts du milieu de la main êtant plus longs que ceux de l'exterieur, la position de repos n'est donc pas avec les doigts bien alignés).
+            <br>
+            <a href="claviers.php?prix_ou_nb_touches=prix&prix=60&nb_touches=60&columnar=on&action=Rechercher">Liste des claviers columnar</a>
+
+            <br> <br> <br> <br>
+            <h3>Les claviers <em>manuform</em></h3>
+            Les claviers <em>manuform</em> sont des claviers dont les touches ne sont pas à plat, mais plutôt en "creux" pour mieux coller à la forme de la main.
+            Les claviers <em>manuform</em> sont presque toujours orthogonaux ou <em>columnar</em>, car cela est plus logique pour suivre le mouvement des doigts.
+            <br>
+            <a href="claviers.php?prix_ou_nb_touches=prix&prix=60&nb_touches=60&manuform=on&action=Rechercher">Liste des claviers manuform</a>
+
         </div>
     </div>
 </body>
