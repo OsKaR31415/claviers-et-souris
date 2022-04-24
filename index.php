38d18aefe85c72d0db68bbc50595e63991aec964
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-04-24 17:10:57 +0200

    add presentation of the site

--- index.php
+++ index.php
@@ -11,7 +11,21 @@
 
     <div class="container">
         <div class="contents">
-            <?php include("lorem_ipsum.inc.php"); ?>
+            <h1>Ergoscar</h1>
+            <h2>Claviers et souris ergonomiques</h2>
+            Bienvenu sur ce site dédié aux <strong>clavier et souris ergonomiques</strong> !
+            <br>
+            <br>
+            Vous pouvez y trouver :
+            <ul>
+                <li><a href="articles.php">Des articles concernant les souris et claviers ergonomiques</a></li>
+                <li><a href="claviers.php">Une page pour rechercher des claviers par critères</a></li>
+                <li><a href="souris.php">Une page pour rechercher des souris par critères</a></li>
+                <li><a href="ajouter.php">Une page pour ajouter un clavier qui n'est pas déjà dans la base de données</a></li>
+            </ul>
+            <!-- foo text to test headers style -->
+            <!-- <?php include("lorem_ipsum.inc.php"); ?> -->
+
         </div>
     </div>
 
